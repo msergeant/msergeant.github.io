f56564e75fecd2d331e4f31e5556ae6a9c89da28
Mike Sergeant <sergeant.mj@gmail.com>
2019-02-15 13:50:32 -0500

    Adding number to English project

---
+++ number-to-english/precache-manifest.b8422a7a635813204ff1ec0afe4c2604.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6af946a46ac006fee2d",
+    "url": "/number-to-english/static/js/runtime~main.f6af946a.js"
+  },
+  {
+    "revision": "82461319e8c2e84a208c",
+    "url": "/number-to-english/static/js/main.82461319.chunk.js"
+  },
+  {
+    "revision": "e6a9378ed2154cfdb7d7",
+    "url": "/number-to-english/static/js/2.e6a9378e.chunk.js"
+  },
+  {
+    "revision": "82461319e8c2e84a208c",
+    "url": "/number-to-english/static/css/main.92ee1a5e.chunk.css"
+  },
+  {
+    "revision": "2827bed0ecbaf4afd2de1ce4772e6032",
+    "url": "/number-to-english/index.html"
+  }
+];
